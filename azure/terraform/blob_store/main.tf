08b30428a8aa1ad9e0fbabe3627860e8de8587b6
Malini Kamala <malinikamala@Malinis-MacBook-Pro.local>
2023-08-04 07:54:53 -0400

    Added provider alias.

--- azure/terraform/blob_store/main.tf
+++ azure/terraform/blob_store/main.tf
@@ -7,7 +7,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.0.2"
-      # configuration_aliases = [azurerm.some_name]
+      configuration_aliases = [azurerm.some_name]
     }
   }
 
